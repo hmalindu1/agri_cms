774ad9726258025f846a6feb943ba69483cc63f3
Hashan <hemachandra.hashan@gmail.com>
2021-07-07 15:58:27 +0530

    set the formatter (prettire) for php + html

--- random.php
+++ random.php
@@ -10,12 +10,14 @@ function return_x_or_o()
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tic-Tac_Toe</title>
 </head>
+
 <body>
     <table>
         <tr>
@@ -35,4 +37,5 @@ function return_x_or_o()
         </tr>
     </table>
 </body>
+
 </html>
