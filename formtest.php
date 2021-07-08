c6c3ab8bbabbc6e922930d7e738c0a1fa80b84f3
Hashan <hemachandra.hashan@gmail.com>
2021-07-08 11:27:37 +0530

    form with post method

---
+++ formtest.php
@@ -0,0 +1,7 @@
+<form action="form_handler.php" method="post">
+    Username: <br>
+    <input type="text" name="username"><br>
+    Password: <br>
+    <input type="password" name="password"><br>
+    <input type="submit" name="submit" value="Submit">
+</form>
