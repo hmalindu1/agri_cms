d6c15845bcddc4c747d4873d433f67a797533959
Hashan <hemachandra.hashan@gmail.com>
2021-07-13 16:02:06 +0530

    including "init.php"

---
+++ includes/init.php
@@ -0,0 +1,10 @@
+<?php
+$dsn = "pgsql:host=localhost;dbname=agri_users;port=5432";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+$pdo = new PDO($dsn, 'postgres', '123', $opt);
+echo "Connected to Database";
+?>
