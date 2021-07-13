750ccf7c7d4c7b6a8af8101d17dacdb73530e6c4
Hashan <hemachandra.hashan@gmail.com>
2021-07-13 12:54:58 +0530

    Checking users in the data base

--- page1.php
+++ page1.php
@@ -10,15 +10,31 @@ echo "Connected to Database";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <?php include "includes/header.php" ?>
-    <body>
-        <?php include "includes/nav.php" ?>
+<?php include "includes/header.php"?>
 
-        <div class="container">
-            <h1 class="text-center">Page 1</h1>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. "</p>
-        </div> <!--Container-->
-        
-        <?php include "includes/footer.php" ?>
-    </body>
+<body>
+    <?php include "includes/nav.php"?>
+
+    <div class="container">
+        <h1 class="text-center">Page 1</h1>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. "</p>
+
+        <br>
+        <?php
+$result = $pdo->query("SELECT * FROM users");
+if ($result->rowCount() > 0) {
+    foreach ($result as $row) {
+        echo $row['firstname'];
+    }
+} else {
+    echo "No users in the users table"; 
+}
+?>
+
+    </div>
+    <!--Container-->
+
+    <?php include "includes/footer.php"?>
+</body>
+
 </html>
