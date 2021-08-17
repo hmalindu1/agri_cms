c790540a369cbf45a38fa32a32e94c63527c795c
Hashan <hemachandra.hashan@gmail.com>
2021-08-17 09:37:53 +0530

    Made some comments inside the code

--- includes/php_functions.php
+++ includes/php_functions.php
@@ -6,7 +6,7 @@ function redirect($loc)
 
 function generate_token()
 {
-    return md5(microtime() . mt_rand());
+    return md5(microtime() . mt_rand()); //Refer core PHP Programming book
 }
 
 function count_field_val($pdo, $tbl, $fld, $val)
@@ -15,20 +15,21 @@ function count_field_val($pdo, $tbl, $fld, $val)
         $sql = "SELECT {$fld} FROM {$tbl} WHERE {$fld}=:value";
         $stmnt = $pdo->prepare($sql);
         $stmnt->execute([':value' => $val]);
-        return $stmnt->rowCount();
+        return $stmnt->rowCount(); // get number of affected rows 'rowCount()'
     } catch (PDOException $e) {
         return $e->getMessage();
     }
 }
 
-function send_mail($to,$subject,$body,$from,$reply){
-    $headers = "From: {$from}" . "\r\n" . "Reply-To: {$reply}" . "\r\n" . "X-Mailer:PHP/".phpversion();
+function send_mail($to, $subject, $body, $from, $reply)
+{
+    $headers = "From: {$from}" . "\r\n" . "Reply-To: {$reply}" . "\r\n" . "X-Mailer:PHP/" . phpversion();
     if ($_SERVER['SERVER_NAME'] != "localhost") {
         # code...
-        mail($to,$subject,$body,$headers);
+        mail($to, $subject, $body, $headers);
     } else {
         # code...
-        echo "<hr><p>To: {$to}</p><p>Subject: {$subject}</p><p>{$body}</p><p>".$headers."</p></hr>";
+        echo "<hr><p>To: {$to}</p><p>Subject: {$subject}</p><p>{$body}</p><p>" . $headers . "</p></hr>";
     }
 }
 
