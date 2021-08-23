60f994f10fda5aefd4049e31347e5bdcf66ef745
Hashan <hemachandra.hashan@gmail.com>
2021-08-23 14:34:06 +0530

    Adding the function

--- activate.php
+++ activate.php
@@ -9,22 +9,22 @@ if (isset($_GET['user'])) {
             try {
                 $stmnt = $pdo->prepare("UPDATE users SET active=1 WHERE username=:username");
                 $stmnt->execute([':username' => $user]);
-                $_SESSION['message'] = "User Activated, Please login to access your content";
+                set_msg("User Activated, Please login to access your content", "success");
                 redirect('index.php');
             } catch (PDOException $e) {
                 echo "Error: {$e}";
             }
         } else {
-            $_SESSION['message'] = "Validation code does not match the database - '{$db_code}'='{$code}'";
+            set_msg("Validation code does not match the database");
             redirect('index.php');
         }
 
     } else {
-        $_SESSION['message'] = "No validation code included with active request";
+        set_msg("No validation code included with activate request");
         redirect('index.php');
     }
 
 } else {
-    $_SESSION['message'] = "No user included with activate request";
+    set_msg("No user included with activate request");
     redirect('index.php');
 }
