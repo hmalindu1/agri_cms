e7ce4c196c6f69b373e3149318023dcb5051c895
Hashan <hemachandra.hashan@gmail.com>
2021-08-23 13:37:48 +0530

    Updating a SESSION variable

--- activate.php
+++ activate.php
@@ -6,8 +6,14 @@ if (isset($_GET['user'])) {
         $code = $_GET['code'];
         $db_code = get_validationcode($user, $pdo);
         if ($code == $db_code) {
-            $_SESSION['message'] = "Codes match, activating user";
-            redirect('index.php');
+            try {
+                $stmnt = $pdo->prepare("UPDATE users SET active=1 WHERE username=:username");
+                $stmnt->execute([':username' => $user]);
+                $_SESSION['message'] = "User Activated, Please login to access your content";
+                redirect('index.php');
+            } catch (PDOException $e) {
+                echo "Error: {$e}";
+            }
         } else {
             $_SESSION['message'] = "Validation code does not match the database - '{$db_code}'='{$code}'";
             redirect('index.php');
