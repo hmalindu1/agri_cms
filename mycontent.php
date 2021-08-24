801ae16bf758bc7499efb296f6ef22a0bae5c204
Hashan <hemachandra.hashan@gmail.com>
2021-08-24 15:32:41 +0530

    My content file

---
+++ mycontent.php
@@ -0,0 +1,27 @@
+<?php include "includes/init.php"?>
+<?php
+    if (isset($_SESSION['username'])) {
+        $username=$_SESSION['username'];
+    } else {
+        redirect('index.php');
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php include "includes/header.php"?>
+    <body>
+        <?php include "includes/nav.php"?>
+
+        <div class="container">
+            <?php
+show_msg();
+?>
+            <h1 class="text-center"><?php echo $username ?>'s content</h1>
+            <p>"Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div> <!--Container-->
+
+        <?php include "includes/footer.php"?>
+    </body>
+</html>
