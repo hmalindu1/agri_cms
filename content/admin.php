a3bbd9d81c7ca8faeb9b3a71b737fb6ae4eb4478
Hashan <hemachandra.hashan@gmail.com>
2021-08-30 13:35:33 +0530

    adding content to the admin page

--- content/admin.php
+++ content/admin.php
@@ -13,11 +13,71 @@
                   <li id="pages" class="tab-label"><a href="#">Pages</a></li>
             </ul>
             <div id='tab-users' class='tab-content'>
+                <?php
+try {
+    $result = $pdo->query("SELECT firstname, lastname, username, email, active, joined, last_login FROM users");
+    if ($result->rowCount() > 0) {
+        echo "<table class=table>";
+        echo "<tr><th>First_Name</th><th>Last_Name</th><th>User_Name</th><th>Email</th><th>Active</th><th>Joined</th><th>Last login</th></tr>";
+        foreach ($result as $row) {
+            if ($row['active']) {
+                $active = "yes";
+            } else {
+                $active = "No";
+            }
+
+            echo "<tr><td>{$row['firstname']}</td><td>{$row['lastname']}</td><td>{$row['username']}</td><td>{$row['email']}</td><td>{$active}</td><td>{$row['joined']}</td><td>{$row['last_login']}</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "No users in the users table";
+    }
+} catch (PDOException $e) {
+    echo "Oops! There was an Error <br><br>" . $e->getMessage() . "<br>";
+    echo "Line Number: " . $e->getLine();
+}
+?>
             </div>
             <div id='tab-groups' class='tab-content'>
+                <?php
+try {
+    $result = $pdo->query("SELECT id, name, descr  FROM groups ORDER BY name");
+    if ($result->rowCount() > 0) {
+        echo "<table class=table>";
+        echo "<tr><th>ID</th><th>Name</th><th>Description</th></tr>";
+        foreach ($result as $row) {
+            echo "<tr><td>{$row['id']}</td><td>{$row['name']}</td><td>{$row['descr']}</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "<br> No groups in the groups table <br><br>";
+    }
+} catch (PDOException $e) {
+    echo "Oops! There was an Error <br><br>" . $e->getMessage() . "<br>";
+    echo "Line Number: " . $e->getLine();
+}
+?>
                 <a href='admin_add_group.php' class="btn btn-success">Add Group</a>
             </div>
             <div id='tab-pages' class='tab-content'>
+                <?php
+try {
+    $result = $pdo->query("SELECT id, name, url, group_id, descr FROM pages ORDER BY name");
+    if ($result->rowCount() > 0) {
+        echo "<table class=table>";
+        echo "<tr><th>ID</th><th>Name</th><th>URL</th><th>Group ID</th></tr>";
+        foreach ($result as $row) {
+            echo "<tr><td>{$row['id']}</td><td>{$row['name']}</td><td>{$row['url']}</td><td>{$row['descr']}</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "<br> No pages in the pages table <br><br>";
+    }
+} catch (PDOException $e) {
+    echo "Oops! There was an Error <br><br>" . $e->getMessage() . "<br>";
+    echo "Line Number: " . $e->getLine();
+}
+?>
                 <a href='admin_add_page.php' class="btn btn-success">Add Page</a>
             </div>
         </div> <!--Container-->
