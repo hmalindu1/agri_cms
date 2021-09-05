d852f538a0c4fe853e0eba1f87a21a8630574829
Hashan <hemachandra.hashan@gmail.com>
2021-09-05 20:45:45 +0530

    added php

--- content/admin_add_page.php
+++ content/admin_add_page.php
@@ -1,33 +1,77 @@
-<?php include("../includes/init.php");?>
+<?php include "../includes/init.php";?>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $descr = $_POST['descr'];
+    $url = $_POST['url'];
+    $group_id = $_POST['group_id'];
+    if (strlen($name) < 3) {
+        $error[] = "Group name must be at least 3 characters";
+    }
+    if (strlen($url) < 3) {
+        $error[] = "Group name must be at least 3 characters";
+    }
+    if (count_field_val($pdo, "groups", "id", "$group_id") == 0) {
+        $error[] = "Group ID not found in group table";
+    }
+
+    if (!isset($error)) {
+        try {
+            $stmnt = $pdo->prepare("INSERT INTO pages (name,descr,url,group_id) VALUES (:name,:descr,:url,:group_id)");
+            $stmnt->execute([":name" => $name, ":descr" => $descr, ":url" => $url, ":group_id" => $group_id]);
+            set_msg("Page '{$name}' has been added", "seccess");
+            redirect("admin.php?tab=pages");
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+} else {
+    $name = "";
+    $descr = "";
+    $url = "";
+    $group_id = "";
+
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
-    <?php include "../includes/header.php" ?>
+    <?php include "../includes/header.php"?>
     <body>
-        <?php include "../includes/nav.php" ?>
+        <?php include "../includes/nav.php"?>
 
         <div class="container">
+            <?php
+show_msg();
+?>
             <div class="row">
                 <div class="col-lg-6 col-lg-offset-3">
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-6 col-md-offset-3">
+                    <?php
+if (isset($error)) {
+    foreach ($error as $msg) {
+        echo "<h4 class='bg-danger text-center'>{$msg}</h4>";
+    }
+}
+?>
                     <div class="panel panel-login">
                         <div class="panel-body">
                             <div class="row">
                                 <div class="col-lg-12">
                                     <form id="register-form" method="post" role="form" >
                                         <div class="form-group">
-                                            <input type="text" name="name" id="name" tabindex="1" class="form-control" placeholder="Page Name" required >
+                                            <input type="text" name="name" id="name" tabindex="1" class="form-control" placeholder="Page Name" value="<?php echo $name ?>"  required >
                                         </div>
                                         <div class="form-group">
-                                            <input type="text" name="url" id="url" tabindex="2" class="form-control" placeholder="Page URL" required >
+                                            <input type="text" name="url" id="url" tabindex="2" class="form-control" placeholder="Page URL" value="<?php echo $url ?>" required >
                                         </div>
                                         <div class="form-group">
-                                            <input type="text" name="group_id" id="group_id" tabindex="3" class="form-control" placeholder="Group ID" required >
+                                            <input type="text" name="group_id" id="group_id" tabindex="3" class="form-control"  placeholder="Group ID" value="<?php echo $group_id ?>" required >
                                         </div>
                                         <div class="form-group">
-                                            <textarea name="descr" id="descr" tabindex="8" class="form-control" placeholder="Description - Tell us about your organization ?"></textarea>
+                                            <textarea name="descr" id="descr" tabindex="8" class="form-control" placeholder="Description - Tell us about your organization ?"><?php echo $descr ?></textarea>
                                         </div>
                                         <div class="form-group">
                                             <div class="row">
@@ -44,6 +88,6 @@
                 </div>
             </div>
         </div>
-        <?php include "../includes/footer.php" ?>
+        <?php include "../includes/footer.php"?>
     </body>
 </html>
