2b7a6ce382cc385cc5bf1d24f531d82809e831e7
Hashan <hemachandra.hashan@gmail.com>
2021-09-07 11:02:53 +0530

    varifying the user and the groups

--- content/admin.php
+++ content/admin.php
@@ -1,4 +1,16 @@
 <?php include "../includes/init.php";?>
+<?php
+if (logged_in()) {
+    $username = $_SESSION['username'];
+    if (!verify_user_group($pdo, $username, "Admin")) {
+        set_msg("User '{$username}' does not have permission to view this page");
+        redirect('../index.php');
+    }
+} else {
+    set_msg("Please log-in and try again");
+    redirect('../index.php');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <?php include "../includes/header.php"?>
