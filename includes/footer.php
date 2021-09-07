a8551a07e1bbf2e43f41951d3f05037745445dd9
Hashan <hemachandra.hashan@gmail.com>
2021-09-07 11:01:59 +0530

    2018 -> 2021

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
         <div class="container">
-            <p class="text-center">&copy; <a href='index.php'>Geotech Hub</a> 2018</p>
+            <p class="text-center">&copy; <a href='index.php'>Geotech Hub</a> 2021</p>
         </div> <!--Container-->
